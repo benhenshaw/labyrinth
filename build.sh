576627f256d1312926da1e4230b182c39da69c47
benhenshaw <benedicthenshaw@gmail.com>
2018-10-10 12:42:08 +0100

    Linked SDL2, created window with pixel buffer

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-cc main.c -o labyrinth
+# macOS / clang
+clang main.c -o labyrinth -framework SDL2
+if [[ $? -eq 0 ]]
+then
+    ./labyrinth
+fi
--- main.c
+++ main.c
@@ -1,1 +1,24 @@
-int main(int argument_count, char ** arguments) {}
+#include <stdbool.h>
+#include <stdint.h>
+typedef uint32_t u32;
+
+#include <SDL2/SDL.h>
+
+int main(int argument_count, char ** arguments)
+{
+    // TODO: Handle failures.
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window * window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    // TODO: Use renderer to display pixel buffer.
+    u32 * pixels = SDL_GetWindowSurface(window)->pixels;
+
+    while (true)
+    {
+        SDL_Event event;
+        if (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT) exit(0);
+        }
+        SDL_UpdateWindowSurface(window);
+    }
+}
